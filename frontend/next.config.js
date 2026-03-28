50f93b46c3832182e43b288ab8b0bf1a73f82f41
sarfrazcodes <sarfrazcodes@gmail.com>
2026-03-28 07:02:05 +0530

    fix next.config parsing and ts error

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,11 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
     ignoreBuildErrors: true,
   },
   eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
